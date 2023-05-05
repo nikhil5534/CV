761854f2d9f6f87c9d708488e05d19a0961e2806
nikhil5534 <78906380+nikhil5534@users.noreply.github.com>
2023-05-05 10:58:08 +0530

    Update index.js

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,73 +1,67 @@
 var nbButton =document.querySelectorAll(".drum").length;
 for (var i = 0 ;i<nbButton; i++){
+
     document.querySelectorAll(".drum")[i].addEventListener("click",function () {
+
     var tap= this.innerHTML;
-    switch (tap) {
+
+    makesound(tap);
+    btani(tap);
+    });
+}
+
+document.addEventListener("keydown",function(event){
+    makesound(event.key);
+    btani(event.key);
+});
+
+
+function makesound(key){
+    switch (key) {
+        
         case "w":
             var w = new Audio('sounds/tom-1.mp3');
             w.play();
             break;
+
         case "a":
             var a = new Audio('sounds/tom-2.mp3');
             a.play();
             break;
+
         case "s":
             var s = new Audio('sounds/tom-3.mp3');
             s.play();
             break;
+
         case "d":
             var d = new Audio('sounds/tom-4.mp3');
             d.play();
             break;
+
         case "j":
             var j = new Audio('sounds/snare.mp3');
             j.play();
             break;
+
         case "k":
             var k = new Audio('sounds/crash.mp3');
             k.play();
             break;
+
         case "l":
             var l = new Audio('sounds/kick-bass.mp3');
             l.play();
             break;
+
         default:
             
     }
-    var keyboardInput = document.getElementById('keyboard-input');
-    document.addEventListener('keypress',(Event)=> {
-        if (Event.key =="w") {
-            var w = new Audio('sounds/tom-1.mp3');
-            w.play();
-        }
-        if (Event.key =="a") {
-            var a = new Audio('sounds/tom-2.mp3');
-            a.play();
-        }
-        if (Event.key =="s") {
-            var s = new Audio('sounds/tom-3.mp3');
-            s.play();
-        }
-        if (Event.key =="d") {
-            var d = new Audio('sounds/tom-4.mp3');
-            d.play();
-        }
-        if (Event.key =="j") {
-            var j = new Audio('sounds/snare.mp3');
-            j.play();
-        }
-        if (Event.key =="k") {
-            var k = new Audio('sounds/crash.mp3');
-            k.play();
-        }
-        if (Event.key =="l") {
-             var l = new Audio('sounds/kick-bass.mp3');
-            l.play();
-        }
-        
-       
-    })
-
-});
-
 }
+function btani(currentKey){
+    var ab=document.querySelector("."+currentKey);
+    ab.classList.add("pressed");
+    setTimeout(function () {
+        ab.classList.remove("pressed");
+    }, 150);
+} 
